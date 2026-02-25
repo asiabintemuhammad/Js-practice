9df556c2ce8daa3bce96864ae44e126f9096b38d
Asia Binte Muhammad <asiabintemuhammad48@gmail.com>
2026-02-25 23:18:05 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,38 @@
+//js
+// ex-1
+function showDate(){
+	document.getElementById('showDate').innerHTML=Date();
+}
+// ex-2
+function sDate(){
+	document.getElementById('sDate').innerHTML=Date();
+}
+// ex-3
+function sText(){
+	document.getElementById('sText').innerHTML="Hello people!";
+}
+// ex-4
+function bulbOn(){
+  document.getElementById('bulb').src="img/bulbon.gif"
+}
+function bulbOff(){
+  document.getElementById('bulb').src="img/bulboff.gif";
+}
+// ex-5
+function styleChange(){
+	document.getElementById('sChange').style.fontSize="50px";
+}
+// ex-6
+function showImg(){
+  document.getElementById('img').style.display="block";
+}
+function hideImg(){
+  document.getElementById('img').style.display="none";
+}
+// ex-6
+function sT(){
+  document.getElementById('s_hText').style.display="block";
+}
+function hT(){
+  document.getElementById('s_hText').style.display="none";
+}
